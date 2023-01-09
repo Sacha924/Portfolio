aeed887fcdc66fee23f14fb0ffa6303c4556429d
Sacha924 <sacha.simon@edu.devinci.fr>
2023-01-09 23:22:57 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="card">
+        <div className="container">
+          <h1 className="title">Gomoku</h1>
+          <img className="imageInCard" src="images/gomoku.png" />
+        </div>
+      </div>
+      <div className="card">
+        <div className="container">
+          <h1 className="title">Gomoku</h1>
+          <img className="imageInCard" src="images/gomoku.png" />
+        </div>
+      </div>
+      <div className="card">
+        <div className="container">
+          <h1 className="title">Gomoku</h1>
+          <img className="imageInCard" src="images/gomoku.png" />
+        </div>
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
