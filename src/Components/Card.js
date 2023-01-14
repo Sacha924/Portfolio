886a7d1513c7b25ac6fc5b56e735febd7d7426ac
Sacha924 <sacha.simon@edu.devinci.fr>
2023-01-14 15:55:32 +0100

    clickable githubLink and iframe

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,49 +1,70 @@
 import "../styles/Card.css";
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 export default function Card(props) {
   const [isRotated, setIsRotated] = useState(false);
+  const [showIframe, setShowIframe] = useState(false);
+  const iframeRef = useRef(null);
+
+  console.log("isRotated: " + isRotated);
 
   const handleClick = () => {
     setIsRotated(!isRotated);
-    console.log("isRotated: " + isRotated);
   };
+  const handleIframe = (e) => {
+    e.preventDefault();
+    e.stopPropagation(); // This method stops the event from propagating up the DOM tree, so the click event on the link will not be passed on to the parent elements, including the card element.
+    setShowIframe(!showIframe);
+  };
+
   return (
-    <div className={`card ${isRotated ? "rotated" : ""}`} onClick={handleClick}>
-      {!isRotated ? (
-        <div>
-          <h1 className="title">{props.project_name}</h1>
-          <img className="imageInCard" src={`images/${props.image}`} />
-          <div className="easterEgg">
-            <a href="https://dino-chrome.com/fr">Easter egg</a>
+    <div>
+      <div className={`card ${isRotated ? "rotated" : ""}`} onClick={handleClick}>
+        {!isRotated ? (
+          <div>
+            <h1 className="title">{props.project_name}</h1>
+            <img className="imageInCard" src={`images/${props.image}`} />
+            <div className="easterEgg">
+              <a href="https://dino-chrome.com/fr">Easter egg</a>
+            </div>
+            <div className="skills">
+              {props.skillDev &&
+                props.skillDev.map((skill) => {
+                  return (
+                    <div className="skill" data-skill={skill}>
+                      {skill}
+                    </div>
+                  );
+                })}
+            </div>
           </div>
-          <div className="skills">
-            {props.skillDev &&
-              props.skillDev.map((skill) => {
-                return (
-                  <div className="skill" data-skill={skill}>
-                    {skill}
-                  </div>
-                );
-              })}
+        ) : (
+          <div className="rotated-container">
+            <h1 className="title">{props.project_name}</h1>
+            <div className="projectDescription" dangerouslySetInnerHTML={{ __html: props.description }} />
+            <a href={props.githubLink} onClick={(e) => handleIframe(e)}>
+              yo le gang
+            </a>
+            <div className="skills">
+              {props.skillDev &&
+                props.skillDev.map((skill) => {
+                  return (
+                    <div className="skill" data-skill={skill}>
+                      {skill}
+                    </div>
+                  );
+                })}
+            </div>
           </div>
-        </div>
-      ) : (
-        <div className="rotated-container">
-          <h1 className="title">{props.project_name}</h1>
-          <div className="projectDescription" dangerouslySetInnerHTML={{ __html: props.description }} />
-          <a href={props.githubLink}>yo le gang</a>
-          <div className="skills">
-            {props.skillDev &&
-              props.skillDev.map((skill) => {
-                return (
-                  <div className="skill" data-skill={skill}>
-                    {skill}
-                  </div>
-                );
-              })}
-          </div>
-        </div>
+        )}
+      </div>
+      {showIframe && (
+        <>
+          <iframe className="myFrame" ref={iframeRef} src={props.githubLink} />{" "}
+          <button className="exitButton" onClick={(e) => handleIframe(e)}>
+            EXIT
+          </button>
+        </>
       )}
     </div>
   );
